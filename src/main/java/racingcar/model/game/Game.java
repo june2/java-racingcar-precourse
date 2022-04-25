be06e1804e08fb4852fefbcf9bc4378fe76b6b76
june2 <>
2022-04-25 16:40:30 +0900

    feat(game,consoleview): view구현, game 플레이 로직 구현

--- src/main/java/racingcar/model/game/Game.java
+++ src/main/java/racingcar/model/game/Game.java
@@ -1,15 +1,38 @@
 package racingcar.model.game;
 
-public class Game {
+public class Game extends GameConfig {
     private boolean isRunning = true;
+    private int count = 0;
 
     private void end() {
         this.isRunning = false;
     }
 
+
+    private void run() {
+        // 게임 시작
+        this.view.inputName();
+        this.player.inputCarNames();
+        this.view.inputCount();
+        this.player.inputCount();
+
+        // race
+        cars.createCarList(this.player.getCarNames());
+        this.view.outputRace();
+        do {
+            cars.race();
+            this.count++;
+            this.view.outputRaceCars(cars.getCars());
+        } while (this.player.getCount() != this.count);
+
+        // output
+        this.view.outputResult(this.cars.getWinnners());
+        this.end();
+    }
+
     public void play() {
         do {
-//            this.run();
+            this.run();
         } while (this.isRunning);
     }
 }
---
+++ src/main/java/racingcar/view/ConsoleView.java
@@ -0,0 +1,33 @@
+package racingcar.view;
+
+import racingcar.constant.Message;
+import racingcar.model.car.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ConsoleView {
+    public void inputName() {
+        System.out.println(Message.INPUT_NAMES);
+    }
+
+    public void inputCount() {
+        System.out.println(Message.INPUT_COUNT);
+    }
+
+    public void outputRace() {
+        System.out.println(Message.OUTPUT_RACE);
+    }
+
+    public void outputRaceCars(List<Car> cars) {
+        cars.forEach((car -> {
+            System.out.println(car.getName() + " : " + car.getPosition());
+        }));
+        System.out.println();
+    }
+
+    public void outputResult(List<Car> winners) {
+        String result = winners.stream().map(Car::getName).collect(Collectors.joining(","));
+        System.out.println(Message.OUTPUT_RESULT + result);
+    }
+}
