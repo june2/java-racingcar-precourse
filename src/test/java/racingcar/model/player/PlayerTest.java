6726c1bbdf8300837bb9e80b9b68c77091aa69c3
june2 <>
2022-04-25 17:00:10 +0900

    fix(playerTest) : play 테스트코드 오류수정

--- src/test/java/racingcar/model/player/PlayerTest.java
+++ src/test/java/racingcar/model/player/PlayerTest.java
@@ -1,14 +1,11 @@
 package racingcar.model.player;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.Application;
-import racingcar.constant.Message;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class PlayerTest extends NsTest {
@@ -30,23 +27,20 @@ public class PlayerTest extends NsTest {
         );
     }
 
-//    @Test
-//    @DisplayName("Player 자동차이름이 5자이상일시, 에러를 던진다.")
-//    void Player_차동자이름_입력에러_검증() {
-//        assertSimpleTest(
-//            () -> {
-//                //given
-//                Player player = new Player();
-//
-//                //when
-//                runException("test1111,test2,test3");
-//                player.inputCarNames();
-//
-//                //then
-//                Assertions.assertThat(output()).contains(Message.ERROR_INVLAID_CAR_NAMES);
-//            }
-//        );
-//    }
+    @Test
+    @DisplayName("Player 자동차이름이 5자이상일시, 에러를 던진다.")
+    void Player_차동자이름_입력에러_검증() {
+        assertThatThrownBy(() -> {
+            //given
+            Player player = new Player();
+
+            //when
+            runException("test1111,test2,test3");
+            player.inputCarNames();
+
+            //then
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 
     @Test
     @DisplayName("Player 시도회수 5입력시, count 값은 5이다.")
@@ -64,20 +58,21 @@ public class PlayerTest extends NsTest {
                 assertThat(player.getCount()).isEqualTo(5);
         });
 }
-//
-//    @Test
-//    @DisplayName("Player 시도회수에 숫자가 아닌 값을 입력시 에러를 던진다.")
-//    void Player_시도횟수_입력에러_검증() {
-//        //given
-//        Player player = new Player();
-//
-//        //when
-//        String input = "5aa";
-//
-//        //then
-//        assertThrows(IllegalArgumentException.class, () ->
-//                player.inputCount(input), Message.ERROR_INVLAID_COUNT);
-//    }
+
+    @Test
+    @DisplayName("Player 시도회수에 숫자가 아닌 값을 입력시 에러를 던진다.")
+    void Player_시도횟수_입력에러_검증() {
+        assertThatThrownBy(() -> {
+            //given
+            Player player = new Player();
+
+            //when
+            runException("5aa");
+            player.inputCount();
+
+            //then
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 
     @Override
     public void runMain() {
