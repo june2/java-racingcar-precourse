f56c6fb6580b88ade249219accc58698910b978a
june2 <>
2022-04-25 17:20:10 +0900

    chore(consoleview) : remove unused import

--- src/main/java/racingcar/view/ConsoleView.java
+++ src/main/java/racingcar/view/ConsoleView.java
@@ -4,7 +4,6 @@ import racingcar.constant.Message;
 import racingcar.model.car.Car;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ConsoleView {
     public void inputName() {
